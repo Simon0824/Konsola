1a8da67cc09eb40d73a95665412bfe8f2813aebf
767Batek <barteklidzbarski9@gmail.com>
2025-03-11 11:26:21 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.Design;
+namespace Zadanie
+{
+    class Mainik
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wpisz liczbe a:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Wpisz liczbe b:");
+            int b = int.Parse(Console.ReadLine());
+            if (a != b)
+            {
+                if(a > b)
+                {
+                    a /= a - b;
+                    Console.WriteLine(a);
+                }
+                else
+                {
+                    b /= b - a;
+                    Console.WriteLine(b);
+                }
+            }
+            else
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
